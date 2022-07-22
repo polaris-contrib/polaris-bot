df69af115f6603155172de4a7b7afea2cada392a
liaochuntao <liaochuntao@live.com>
2022-07-22 18:34:48 +0800

    feat: 优化issue report内容格式

--- date.js
+++ date.js
@@ -4,8 +4,8 @@ module.exports = {
     headDate: () => moment.utc().set({
         hour: 0, minute: 0, second: 0, millisecond: 0,
     }).format(),
-    tailDate: () => moment.utc().set({
+    tailDate: (index= 7) => moment.utc().set({
         hour: 0, minute: 0, second: 0, millisecond: 0,
-    }).subtract(7, 'days').format(),
+    }).subtract(index, 'days').format(),
     getDayBeforeDate: (date) => moment(date).subtract(1, 'days').format(),
 };
--- index.js
+++ index.js
@@ -24,27 +24,33 @@ module.exports = (app) => {
         const {owner, repo} = context.repo()
         console.log("owner : " + owner + " repo : " + repo)
 
-        // 查询一周之前的 issue 记录信息
-        const tailDateObject = moment(date.tailDate()).format("YYYY-MM-DDTHH:MM:SSZ");
-        const queryWeeklyIssue = {
+        // 查询上一周的 issue 记录信息
+        const lastWeekIssues = await listIssue(context, {
             owner: owner,
             repo: repo,
             state: 'all',
-            since: tailDateObject,
+            since: moment(date.tailDate(14)).format("YYYY-MM-DDTHH:MM:SSZ"),
             per_page: 100,
-        }
-        const issues = await listIssue(context, queryWeeklyIssue)
+        })
+
+        // 查询一周之内的 issue 记录信息
+        const curWeekIssues = await listIssue(context, {
+            owner: owner,
+            repo: repo,
+            state: 'all',
+            since: moment(date.tailDate()).format("YYYY-MM-DDTHH:MM:SSZ"),
+            per_page: 100,
+        })
 
         // 查询打上了 bug 标签并且仍然处于 open 状态的 issue
-        const queryAllBugIssue = {
+        const totalBugIssues = await listIssue(context, {
             owner: owner,
             repo: repo,
             state: 'open',
             per_page: 100,
-        }
-        const totalBugIssues = await listIssue(context, queryAllBugIssue)
+        })
 
-        const comment = buildIssueComment(issues, totalBugIssues)
+        const comment = buildIssueComment(curWeekIssues, lastWeekIssues, totalBugIssues)
 
         const newComment = context.issue({ body: comment });
         context.octokit.issues.createComment(newComment)
@@ -69,14 +75,19 @@ async function listIssue(ctx, params) {
     return issues
 }
 
-function buildIssueComment(issues, totalBugIssues) {
+function buildIssueComment(issues, lastWeekIssues, totalBugIssues) {
 
     let body = `
 # Weekly-Report (${moment().format("YYYY-MM-DD")})
     `
     let openBugIssueStr = '\n## New Bug (新增的 bug)\n';
-    let closeBugIssueStr = '\n## Close Bug (已关闭的 bug)\n';
+    let closeBugIssueStr = '\n## Close Bug (关闭的 bug)\n';
 
+    let openIssueStr =  "\n## New Issue (新增的 issue)\n";
+    let closeIssueStr = "\n## Close Issue (关闭的 issue)\n";
+
+
+    // 计算当前仍然处于打开的 bug issue 数量
     const openBugIssue = issues.filter((item) => item.state === 'open').filter((item) => {
         let isBug = false
         item.labels.forEach((l) => {
@@ -94,6 +105,7 @@ function buildIssueComment(issues, totalBugIssues) {
         });
     }
 
+    // 记录本周关闭的 bug issue 数量
     const closeBugIssue = issues.filter((item) => item.state === 'closed').filter((item) => {
         let isBug = false
         item.labels.forEach((l) => {
@@ -111,9 +123,69 @@ function buildIssueComment(issues, totalBugIssues) {
         });
     }
 
-    let table = `
+
+    // 记录本周关闭的 issue 数量
+    const closeIssue = issues.filter((item) => item.state === 'closed');
+    if (closeIssue.length > 0) {
+        closeIssue.forEach((item) => {
+            closeIssueStr += `- :yum: [#${item.number}](${item.html_url}) ${item.title.replace(/\n/g, ' ')}\n`;
+        });
+    }
+
+    // 记录本周新增的 issue 数量
+    const openIssue = issues.filter((item) => item.state === 'open');
+    if (openIssue.length > 0) {
+        openIssue.forEach((item) => {
+            openIssueStr += `- :bug: [#${item.number}](${item.html_url}) ${item.title.replace(/\n/g, ' ')}\n`;
+        });
+    }
+
+    let issueCategory = `
 \n
 ## Issue 情况
+
+### issue 变化
+
+| 本周新增 Issue 数量 | 本周关闭 Issue 数量 |
+| :--: | :--: |
+| ${openIssue.length} | ${closeIssue.length} |
+\n
+
+### issue 类别
+
+| Issue 标签 | 本周新增数量 |
+| :-- | :--: |`
+
+    const expectLabel = new Map();
+    expectLabel.set("bug", "缺陷")
+    expectLabel.set("enhancement", "优化 or 特性")
+    expectLabel.set("documentation", "文档")
+    expectLabel.set("code quality", "代码质量")
+    expectLabel.set("need discuss", "待讨论")
+
+    const labelCategory = new Map();
+    labelCategory.set("bug", 0)
+    labelCategory.set("enhancement", 0)
+    labelCategory.set("documentation", 0)
+    labelCategory.set("code quality", 0)
+    labelCategory.set("need discuss", 0)
+
+    openIssue.forEach((issue) => {
+        issue.labels.forEach((label) => {
+            if (expectLabel.has(label)) {
+                let v = labelCategory.get(label)
+                labelCategory.set(label, v + 1)
+            }
+        })
+    })
+
+    labelCategory.forEach((value, key) => {
+        issueCategory += `\n| **${key} (${expectLabel.get(key)})** | ${value} |`
+    })
+
+    let issueBugInfo = `
+\n
+## Bug 情况
 
 | 未关闭的 Bug 数量 | 本周新增 Bug 数量 | 本周关闭 Bug 数量 |
 | :--: | :--: | :--: |
@@ -121,9 +193,11 @@ function buildIssueComment(issues, totalBugIssues) {
 \n
     `
 
-    body += table
+    body += issueCategory
+    body += issueBugInfo
     body += openBugIssueStr
     body += closeBugIssueStr
+    body += closeIssueStr
 
     return body
 }
